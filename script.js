3242f5876cb32697d2ad3b14e9739ea9dda983a2
Br0cod3 <thespammingbott@gmail.com>
2025-07-08 14:51:15 +0100

    added some minor adjustments

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
 const container = document.querySelector(".container");
-// const userInput = prompt("Enter in any number of your choosing: ")
+const button = document.querySelector(".btn");
 
-function sixteenRows() {
-  for (let x = 0; x < 16; x++) {
-    const rowContainer = document.createElement("div")
-    rowContainer.classList.add("row")
-    container.appendChild(rowContainer)
-    for (let i = 0; i < 16; i++) {
-        const square = document.createElement("div");
-        square.classList.add("square");
-        rowContainer.appendChild(square);
+function columnRows(num) {
+  for (let x = 0; x < num; x++) {
+    const rowContainer = document.createElement("div");
+    rowContainer.classList.add("row");
+    container.appendChild(rowContainer);
+    for (let i = 0; i < num; i++) {
+      const square = document.createElement("div");
+      square.classList.add("square");
+      rowContainer.appendChild(square);
     }
   }
 }
 
-sixteenRows();
+function removeGrid() {
+  const row = document.querySelectorAll(".row");
+  for (const child of row) {
+    container.removeChild(child);
+  }
+}
 
+function resizeGrid() {
+  userInput = prompt("What's your desired grid size?");
+  if (isNaN(userInput) || userInput < 1 || userInput > 100) {
+    alert("Enter a valid number between 1 and 100");
+    return;
+  } else {
+    removeGrid();
+    columnRows(userInput);
+  }
+}
+
+columnRows(16);
+button.addEventListener("click", resizeGrid);
